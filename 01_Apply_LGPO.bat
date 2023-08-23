d9719e34c72f4ef7b84ba062fdc43a74c6f385ef
Yossi Doctor <yossidoctor98@gmail.com>
2023-08-23 14:13:12 +0300

    Late August Patch

--- 01_Apply_LGPO.bat
+++ 01_Apply_LGPO.bat
@@ -12,22 +12,14 @@ if %ErrorLevel% NEQ 0 (
 
 echo Deleting current policies
 for %%D in (GroupPolicy, GroupPolicyUsers) do (
-    RD /S /Q "%WinDir%\System32\%%D" >NUL 2>&1
+    if exist "%WinDir%\System32\%%D" RD /S /Q "%WinDir%\System32\%%D" >NUL 2>&1
 )
 
-:: Setting exploit mitigation policy
-echo D | XCopy DOD_EP_V3.xml "%SystemDrive%\MitigationPolicy" /I /Y /Q >NUL 2>&1
-XCopy SiPolicy.p7b "%SystemDrive%\MitigationPolicy" /I /Y /Q >NUL 2>&1
+echo Importing exploit mitigation policy
+echo D | XCopy "DOD_EP_V3.xml" "%SystemDrive%\MitigationPolicy" /I /Y /Q >NUL 2>&1
 
-:: Unzipping archives containing the ADMX policiy definitions
-for /f "tokens=*" %%a in ('dir /a:-d /b ^| findstr /r ".zip$"') do (
-    tar -xf %%a >NUL 2>&1
-)
-:: Importing ADMX policiy definitions to Windir
-robocopy "PolicyDefinitions\" "%WinDir%\PolicyDefinitions\" /E /B /XX /IS /NFL /NDL /NJH /NJS /NC /NS /NP /copyall >NUL 2>&1
-
-:: Deleting unzipped local policy files
-RD /s /q "PolicyDefinitions" >NUL 2>&1
+echo Importing policiy definitions
+robocopy "PolicyDefinitions" "%WinDir%\PolicyDefinitions" /E /B /XX /IS /NFL /NDL /NJH /NJS /NC /NS /NP /copyall >NUL 2>&1
 
 echo Setting machine and user group policies
 lgpo.exe /m machine_registry.pol /q >NUL 2>&1
