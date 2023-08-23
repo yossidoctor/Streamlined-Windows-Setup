525dc8f3c8f3b24c57c898b688f01c7e44836124
Yossi Doctor <yossidoctor98@gmail.com>
2023-08-23 14:13:12 +0300

    Late August Patch
    Former-commit-id: f8959fcaa012b6bcc6d8900d24d2dcc453c44919 [formerly 3150d9e4ae5ed09b7939f449a1dd7e6c1d528d3b] [formerly 1c5ddb6b7a2256e5d3a79f824670a0d46b0611bb] [formerly f83dcecbd60f56f9b85a52a73bd4449a3f7be3b4]
    Former-commit-id: 4816e0c999ef730496b0a466a1f8ca984233fa65
    Former-commit-id: 13a9bb2e5d645ecd04a4671a633bedfa69d7c064
    Former-commit-id: 78cdf1f0eb120f580c4d2deaed8660e4ef0d9224
    Former-commit-id: fcdbc0137538d0624fc960d9cc6a13295038b33c

---
+++ 01_Apply_LGPO.bat
@@ -0,0 +1,33 @@
+@echo off
+
+pushd %~dp0\Automated\LGPO
+
+net session >NUL 2>&1
+
+if %ErrorLevel% NEQ 0 (
+    echo Permission denied. This script must be run as an Administrator.
+    pause
+    exit
+)
+
+echo Deleting current policies
+for %%D in (GroupPolicy, GroupPolicyUsers) do (
+    if exist "%WinDir%\System32\%%D" RD /S /Q "%WinDir%\System32\%%D" >NUL 2>&1
+)
+
+echo Importing exploit mitigation policy
+echo D | XCopy "DOD_EP_V3.xml" "%SystemDrive%\MitigationPolicy" /I /Y /Q >NUL 2>&1
+
+echo Importing policiy definitions
+robocopy "PolicyDefinitions" "%WinDir%\PolicyDefinitions" /E /B /XX /IS /NFL /NDL /NJH /NJS /NC /NS /NP /copyall >NUL 2>&1
+
+echo Setting machine and user group policies
+lgpo.exe /m machine_registry.pol /q >NUL 2>&1
+lgpo.exe /u user_registry.pol /q >NUL 2>&1
+gpupdate /force
+
+echo.
+echo.
+echo DONE.
+pause
+exit
