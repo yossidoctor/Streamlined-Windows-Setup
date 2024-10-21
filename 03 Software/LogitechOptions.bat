0c69df50c4089135e49df9312063fb48748871e6
Yossi Doctor <yossidoctor98@gmail.com>
2024-10-21 13:18:23 +0300

    fix: software scripts working directory & admin privileges

--- 03 Software/LogitechOptions.bat
+++ 03 Software/LogitechOptions.bat
@@ -1,4 +1,16 @@
 @echo off
+
+:: Changes the working directory to the script directory
+pushd %~dp0
+
+:: Check if the script is running with administrative privileges
+net session >NUL 2>&1
+if %ErrorLevel% NEQ 0 (
+    echo Permission denied. This script must be run as an Administrator.
+    pause
+    exit /b 1
+)
+
 set "desktop=%USERPROFILE%\Desktop"
 curl -L -o "%desktop%\logitech_options_installer.exe" https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe
 echo Download completed!
--- 03 Software/f.lux.bat
+++ 03 Software/f.lux.bat
@@ -1,4 +1,16 @@
 @echo off
+
+:: Changes the working directory to the script directory
+pushd %~dp0
+
+:: Check if the script is running with administrative privileges
+net session >NUL 2>&1
+if %ErrorLevel% NEQ 0 (
+    echo Permission denied. This script must be run as an Administrator.
+    pause
+    exit /b 1
+)
+
 set "desktop=%USERPROFILE%\Desktop"
 curl -L -o "%desktop%\flux_installer.exe" https://justgetflux.com/dlwin.html
 echo Download completed!
