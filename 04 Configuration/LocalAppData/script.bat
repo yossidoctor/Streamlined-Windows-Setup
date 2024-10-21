44c16b18d164028b5ca699c1a497e16b843c39d5
Yossi Doctor <yossidoctor98@gmail.com>
2024-10-21 13:20:55 +0300

    add: localappdata and terminal app settings

---
+++ 04 Configuration/LocalAppData/script.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Changes the working directory to the script directory
+pushd %~dp0
+
+:: Check if the script is running with administrative privileges
+net session >NUL 2>&1
+if %ErrorLevel% NEQ 0 (
+    echo Permission denied. This script must be run as an Administrator.
+    pause
+    exit /b 1
+)
+
+echo.
+echo Copying LocalAppData folders
+robocopy . "%LocalAppData%" /E /B /IS /IT /copyall /XF script.bar >NUL 2>&1
+
+echo.
+echo.
+echo DONE.
+pause
+
