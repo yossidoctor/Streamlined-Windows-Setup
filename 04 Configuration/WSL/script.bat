e1ed0067cf73b1c24d986eec6cf075f10aae757a
Yossi Doctor <yossidoctor98@gmail.com>
2024-10-21 13:20:00 +0300

    add: wsl setup script

---
+++ 04 Configuration/WSL/script.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Changes the working directory to the script directory
+pushd %~dp0
+
+:: Check if the script is running with administrative privileges
+net session >NUL 2>&1
+if %ErrorLevel% NEQ 0 (
+    echo Permission denied. This script must be run as an Administrator.
+    pause
+    exit /b 1
+)
+
+wsl git clone https://github.com/yossidoctor/linux-setup.git ~/linux-setup
+wsl rsync -av --exclude='.git' --exclude='README.MD' linux-setup/ ./
+wsl sh setup.sh
+pause
